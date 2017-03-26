04e6554ae23002ba0737ca454a7eedc0b5485a0e
Paul Schweizer <paulschweizer@gmx.net>
2017-03-25 20:54:19 -0700

    Fixed tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,30 +1,31 @@
 import mock
 import unittest
 
-from tinypipeline.core import config
 from tinypipeline.core.project import Project
 
 
 class TestProject(unittest.TestCase):
-
     """Project class."""
 
     project_name = 'tinypipeline'
 
-    def test_project(self):
+    @mock.patch('tinypipeline.core.project.open', new_callable=mock.mock_open,
+                read_data='{"description": "test", "template": "test"}')
+    def test_project(self, mock_open):
         """Initialize a project."""
         project = Project(self.project_name)
-        self.assertNotEqual('', project.name)
-        self.assertNotEqual('', project.description)
-        self.assertNotEqual('', project.template)
-        self.assertIn(project.name, str(project))
-        self.assertIn(project.description, str(project))
-        self.assertIn(project.template, str(project))
+        self.assertEqual(project.name, TestProject.project_name)
+        self.assertEqual(project.description, 'test')
+        self.assertEqual(project.template, 'test')
+        self.assertIn(TestProject.project_name, str(project))
+        self.assertIn('test', str(project))
     # end def test_project
 
+    @mock.patch('tinypipeline.core.project.open', new_callable=mock.mock_open,
+                read_data='{"description": "", "template": ""}')
     @mock.patch('os.path.exists', return_value=True)
     @mock.patch('os.listdir', return_value=['tinypipeline'])
-    def test_find_projects(self, listdir, exists):
+    def test_find_projects(self, listdir, exists, mock_open):
         """Get all available projects."""
         projects = Project.find_projects()
         self.assertIn(self.project_name, [p.name for p in projects])
