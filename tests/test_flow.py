767e7bdb5f6f09ffbb9946d79f2543f8a4ab1c10
Paul Schweizer <paulschweizer@gmx.net>
2017-04-07 16:42:05 -0700

    Update test_flow.py

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -241,7 +241,45 @@ e.evaluate()
 
 
 
+class ValueNode(FlowNode):
 
+    flow_ins = ['value']
+    flow_outs = ['value']
+
+    def __init__(self, value=None):
+        super(ValueNode, self).__init__()
+        self.value = value
+
+    def compute(self):
+        pass
+
+
+class EmailNode(FlowNode):
+
+    flow_ins = ['recipients', 'text']
+
+    def __init__(self):
+        super(EmailNode, self).__init__()
+        self.text = ''
+
+    def compute(self):
+        print 'Sending Email to:', self.recipients, self.text
+
+
+r = ValueNode(['Me', 'You', 'Her'])
+t = ValueNode('The Text of the email')
+e = EmailNode()
+
+
+r.connect('value', e, 'recipients')
+t.connect('value', e, 'text')
+
+
+engine = FlowEngine()
+engine.nodes = [r, t, e]
+
+
+engine.evaluate()
 
 
 
